cf3c269f2ec716a1e80cada58e298be35c769f0d
ben leer <boaz242@gmail.com>
2018-02-16 14:49:01 -0600

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'products/index'
+
   resources :contacts, only: [:new, :create]
   root 'welcome#index'
+  get 'welcome/about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
