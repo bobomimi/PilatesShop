a08f4196c311b49cb8ae64cb39a0c70e0f04c992
ben leer <boaz242@gmail.com>
2018-02-21 09:35:43 -0600

    init

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180220155922) do
+ActiveRecord::Schema.define(version: 20180218201258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20180220155922) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
+    t.string "sub_title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -37,7 +38,6 @@ ActiveRecord::Schema.define(version: 20180220155922) do
     t.integer "image_file_size"
     t.datetime "image_updated_at"
     t.string "slug"
-    t.string "sub_title"
     t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
@@ -69,10 +69,8 @@ ActiveRecord::Schema.define(version: 20180220155922) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "slug"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["slug"], name: "index_users_on_slug", unique: true
   end
 
 end
