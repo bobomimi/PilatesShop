c0bde8885fe6b4891ece9a45406543c4f9bda35b
ben leer <boaz242@gmail.com>
2018-02-23 10:03:35 -0600

    init

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,8 @@
 module PostsHelper
-	def cache_key_for_post
-		count          = Post.count
-		max_updated_at = Post.maximum(:updated_at).try(:utc).try(:to_s, :number)
-		"post/all-#{count}-#{max_updated_at}"
-	end
+  def cache_key_for_post
+   count          = Post.count	
+   max_updated_at = Post.maximum(:updated_at).try(:utc).try(:to_s, :number)
+   "post/all-#{count}-#{max_updated_at}"
+  end
 end
+
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,4 @@ class Post < ApplicationRecord
   friendly_id :title, use: :slugged
   has_attached_file :image, styles: { post_index: "750x300", large: "600x600", medium: "300x300>", thumb: "100x100#{}>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-  after_save    :expire_contact_all_cache
-  after_destroy :expire_contact_all_cache
-  def self.all_cached
-    Rails.cache.fetch('Post.all') { all.to_a }
-  end
 end
