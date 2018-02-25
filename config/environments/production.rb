f86b73d85a6b7482c6fff7729ef34404f7f0a909
ben leer <boaz242@gmail.com>
2018-02-25 15:27:14 -0600

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,5 +111,8 @@ Rails.application.configure do
     'Cache-Control' => "public, s-maxage=#{365.days.to_i}, maxage=#{180.days.to_i}",
     'Expires' => "#{1.year.from_now.to_formatted_s(:rfc822)}"
   }
+
+  config.assets.css_compressor = :yui
   
+  config.assets.js_compressor = :uglifier
 end
