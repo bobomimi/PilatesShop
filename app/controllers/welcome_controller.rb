06b0c6b5f87846dc1efdb82c7f2fc5a3e74a18e6
ben leer <boaz242@gmail.com>
2018-02-27 11:36:48 -0600

    init

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-  	@products = Product.paginate(:page => params[:page], :per_page => 3)
   	@posts = Post.paginate(:page => params[:page], :per_page => 3)
+  	@products = Product.paginate(:page => params[:page], :per_page => 3)
   end
 
   def about
