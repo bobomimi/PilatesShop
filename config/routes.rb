d922840289329ac39b046fba11d9d442f42de7f1
ben leer <boaz242@gmail.com>
2018-03-01 13:17:32 -0600

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
+  scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
+  
+    devise_for :admins
+    mount Ckeditor::Engine => '/ckeditor'
+    resources :products
+    resources :posts 
+    resources :charges
+    resources :contacts, only: [:new, :create]
+    root 'welcome#index'
+    get 'welcome/about'
+    get '*path' => redirect('/')
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
 
-  devise_for :admins
-  mount Ckeditor::Engine => '/ckeditor'
-  resources :products
-  resources :posts 
-  resources :charges
-  resources :contacts, only: [:new, :create]
-  root 'welcome#index'
-  get 'welcome/about'
-  get '*path' => redirect('/')
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
