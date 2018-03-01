599005ea5f9e7b322c352467f3811b080a7161e3
ben leer <boaz242@gmail.com>
2018-03-01 13:27:59 -0600

    init

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module PilatesShop
     config.serve_static_assets = true
     config.middleware.use Rack::Deflater
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :es
+    config.i18n.default_locale = :en
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
